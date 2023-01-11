dc39e57de6ecf5fb865793ae82d15a5bcc1315e4
Niajete <105147485+Niajete@users.noreply.github.com>
2023-01-11 07:02:59 +0100

    Add files via upload

---
+++ Konstant.py
@@ -0,0 +1,22 @@
+WIDTH = 600 #Pixel
+HEIGHT = 600
+
+ROWS = 3
+COLS = 3
+
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# Farben (rgb  -- rot, grün, blau)
+BG_Color = (28,170, 156)
+LINE_Color = (23, 145, 135)
+CIRC_Color = (239, 231, 200)
+CROSS_Color = (66, 66, 66)
+Menu_Color = (204, 204, 255)
